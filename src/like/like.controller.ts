307993832a1016285b71a8c74d7aa777054af0e2
snd <snd@MacBook-Air-snd.local>
2024-04-18 14:14:05 +0300

    add openapi description

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,14 +1,28 @@
+import { ApiException } from '@nanogiants/nestjs-swagger-api-exception-decorator';
 import { Controller, Param, Patch } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 
+import { UserNotAuthorizedException } from '#auth/exceptions/auth.exceptions';
+import { PostNotFoundException } from '#post/exceptions/post.exceptions';
 import { User } from '#utils/decorators';
 import { ParamIdValidationPipe } from '#utils/pipes';
 
 import { LikeService } from './like.service';
 
+@ApiTags('Лайки')
+@ApiBearerAuth()
 @Controller('like')
 export class LikeController {
   constructor(private readonly likeService: LikeService) {}
 
+  @ApiOkResponse()
+  @ApiParam({ name: 'postId', type: Number, example: 1 })
+  @ApiException(() => [UserNotAuthorizedException, PostNotFoundException])
   @Patch(':postId')
   create(
     @Param('postId', ParamIdValidationPipe) postId: number,
--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
+import { PostNotFoundException } from '#post/exceptions/post.exceptions';
+import { PostService } from '#post/post.service';
 import { PrismaService } from '#prisma/prisma.service';
 
 import { CheckLikeParams, LikePostParams } from './types/like.types';
 
 @Injectable()
 export class LikeService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly postService: PostService,
+  ) {}
 
   async likePost({ userId, postId }: LikePostParams): Promise<void> {
+    const post = await this.postService.findPostById(postId);
+
+    if (!post) throw new PostNotFoundException();
+
     const like = await this.checkLike({ postId, userId });
 
     if (like) {
