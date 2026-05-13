4b35c3e67814a5d2e3016ba9d89ca66f0f31087b
Sharkov Nikita <bigbasspoll@gmail.com>
2026-05-13 19:20:48 +0300

    fix: fixed cloudinary returning

--- src/core/cloudinary.provider.ts
+++ src/core/cloudinary.provider.ts
@@ -7,10 +7,12 @@ export const CloudinaryProvider = {
   provide: CLOUDINARY,
   inject: [ConfigService],
   useFactory: (configService: ConfigService) => {
-    return cloudinary.config({
+    cloudinary.config({
       cloud_name: configService.get('CLOUDINARY_CLOUD_NAME'),
       api_key: configService.get('CLOUDINARY_API_KEY'),
       api_secret: configService.get('CLOUDINARY_API_SECRET'),
     });
+
+    return cloudinary;
   },
 };
